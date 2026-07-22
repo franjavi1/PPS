4c740595b787c89fe9531196d250dad7678657e7
Maradoo76 <diego_rg@hotmail.com>
2026-07-22 00:41:48 -0300

    Modalidades listo

---
+++ backend/models/modalidades.py
@@ -0,0 +1,48 @@
+from datetime import datetime
+
+from db import db
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Integer, String, DateTime, func
+
+
+class Modalidades(db.Model):
+    # Nombre de la tabla asociada en la base de datos
+    __tablename__ = "Modalidades"
+
+    # ID principal de la modalidad
+    modalidadesid: Mapped[int] = mapped_column(
+        Integer,
+        primary_key=True,
+        autoincrement=True
+    )
+
+    # Descripcion de la modalidad
+    descripcion: Mapped[str] = mapped_column(
+        String(45),
+        nullable=False
+    )
+
+    # Usuario que realizo la ultima accion sobre el registro
+    usuario_accion: Mapped[int] = mapped_column(
+        "usuarioAccion",
+        Integer,
+        nullable=False
+    )
+
+    # Fecha y hora de creacion del registro
+    ts_creacion: Mapped[datetime] = mapped_column(
+        "tsCreacion",
+        DateTime,
+        server_default=func.now(),
+        nullable=False
+    )
+
+    # Fecha y hora de la ultima modificacion del registro
+    ts_modificacion: Mapped[datetime] = mapped_column(
+        "tsModificacion",
+        DateTime,
+        server_default=func.now(),
+        onupdate=func.now(),
+        nullable=False
+
+    )
