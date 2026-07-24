4379cbbbf49913f2afe1fb6758ea98eb706fd283
Maradoo76 <diego_rg@hotmail.com>
2026-07-24 01:11:51 -0300

    Cambio menor en la ruta del api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 
 import toast from "react-hot-toast";
 
-export const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8000/api/g1";
+export const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8000/planes";
 
 
 export async function apiRequest(path, options = {}) {
