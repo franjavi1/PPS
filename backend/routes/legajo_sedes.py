a907078f397271c38eced730d77cd39dfef89ef0
Lucas Zabala <lucas22nic@gmail.com>
2026-08-02 20:23:22 -0300

    correccion editar y ver legajo

--- backend/routes/legajo_sedes.py
+++ backend/routes/legajo_sedes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request
-from schemas.legajo_sedes_schema import legajo_sedes_schema
+from schemas.legajo_sedes_schema import legajo_sedes_schema, legajos_sedes_schema
 from utils.utilidades import respuesta_api
 from utils.errores import APIError
 from auth_common.decorador import requires_permission
@@ -19,7 +19,7 @@ legajo_sedes_bp = Blueprint("legajo_sedes_bp", __name__, url_prefix="/legajo-sed
 @requires_permission("planes.legajo_sedes.ver")
 def get_legajos_sedes():
     legajos_sedes = obtener_todos()
-    data = legajo_sedes_schema.dump(legajos_sedes)
+    data = legajos_sedes_schema.dump(legajos_sedes)
 
     if len(data) == 0:
         return respuesta_api(True, [], "No se encontraron resultados")
